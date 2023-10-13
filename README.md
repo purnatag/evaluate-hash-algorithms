84f73cd86e9893674c878dbeb620ca8ee7429124
Purnata Ghosal <purnatag@gmail.com>
2023-10-13 12:24:30 +0530

    add: tlsh log

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# eval_compression : Evaluation Pipeline for TLSH and other hashing schemes
+# eval_compression : Evaluation Pipeline for TLSH (and other hashing schemes)
 ---
 
 This repository contains the file `eval.py`, used to evaluate the quality of compression exhibited by the hashing schemes `tlsh`, `ssdeep` and `sdhash` on system event signature logs.
-Currently the only metric for evaluation is computation of locality.
+The metrics for evaluation are --
+- Computation of locality or change ratio which is %change in hash/%change in the input signature.
+- Mean gaps between the changes in the hash value, and variance
+- Mean and variance of the distribution of deltas on modified input signatures versus Mean and variance of deltas on hash values. Here ~deltas~ refers to % change per original signature length and % change per original hash length, respectively.
--- eval.py
+++ eval.py
@@ -9,10 +9,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Program computes locality of chosen hash function on p0f signature log file
+# Currently supporting only TLSH
+
 
 # Choose required hash function and compute hash
-
-
 def compute_hash(sign, ch):
     if ch == 1:
         sign_hash = compute_tlsh(sign)
@@ -72,17 +72,21 @@ def change_per_pos(sign, mod_sign, sign_hash, mod_sign_hash, delta_input_list, d
 
 # Calculate mean gap between two changes in the hash, and variance
 def calculate_gap_stats(mean_gap, var_gap, num, sign_hash, mod_sign_hash):
-    len = min(len(sign_hash), len(mod_sign_hash))
     prev_i = -1
+    i = 0
+    j = 0
     gap_sum = 0
     gap_num = 0
-    for i in range(len):
-        if sign_hash[i] != mod_sign_hash[i]:
+    while i < len(sign_hash) and j < len(mod_sign_hash):
+        if sign_hash[i] != mod_sign_hash[j]:
             if prev_i == -1:
                 prev_i = i
             else:
                 gap_sum += i - prev_i
                 gap_num += 1
+        else:
+            i += 1
+        j += 1
     curr_mean = (float)(gap_sum/gap_num)
     mean_gap += (curr_mean - mean_gap)/(num + 1)
     var_gap = compute_var_online(mean_gap, var_gap, num, curr_mean)
@@ -112,66 +116,29 @@ def rand_change(sign):
     return mod_sign
 
 
-# main function
-def main():
-    input_file = "..\p0f signatures\p0f2.log"
-    print("Choose the hashing algorithm to run:\n ")
-    print(" 1. TLSH \n 2. ssdeep \n 3. sdhash\n")
-    ch = int(input("Enter choice:"))
-
-    f = open(input_file, '+r')
-    if ch == 1:
-        output_file = open("tlsh_log.txt", 'w+')
-    elif ch == 2:
-        output_file = open("ssdeep_log.txt", 'w+')
-    elif ch == 3:
-        output_file = open("sdhash_log.txt", 'w+')
-    else:
-        print("Invalid input. Exiting.\n")
-        sys.exit(0)
-
-    lines = f.readlines()
-    f.close()
-
-    # number of modified signatures on which gap stats have been calculated
-    num = 0
-    mean_gap = 0
-    var_gap = 0
-
-    # lists for plotting
-    delta_input_list = []
-    delta_hash_list = []
-
-    for line in lines:
-        sign = line.strip()
-        output_file.write("Current sign: " + sign + '\n')
-        sign_hash = compute_hash(sign, ch)
-        print("Processing "+sign_hash+"\n")
-        for _ in range(1, 20):
-            mod_sign = rand_change(sign)
-            # Calculating modified hash
-            mod_sign_hash = compute_hash(mod_sign, ch)
-            # Calculating locality
-            change_ratio = change_per_pos(
-                sign, mod_sign, sign_hash, mod_sign_hash, delta_input_list, delta_hash_list)
-            output_file.write(mod_sign + " Change ratio: " +
-                              str(change_ratio) + '\n')
-            # (mean_gap, var_gap) = calculate_gap_stats(
-            #    mean_gap, var_gap, num, sign_hash, mod_sign_hash)
-            num += 1
-
-    output_file.close()
-
-    # Plot the deltas in a scatter plot, where Y-axis represents change (delta) in input
-    # and X-axis represents delta in hash
+# Plotting the deltas in a scatter plot, where Y-axis represents change (delta) in input
+# and X-axis represents delta in hash. Plotting the change ratio in the same plot
+def get_plots(fname, crlist, delta_input_list, delta_hash_list):
+    print("Plotting the data...")
     # X and Y axes values
-    x = np.array(delta_hash_list)
+    x1 = np.array(crlist)
+    x2 = np.array(delta_hash_list)
     y = np.array(delta_input_list)
 
-    # Scaling the axes
-    fig, ax = plt.subplots()
-    ax.axline((0, 0), slope=1)
-    ax.scatter(x, y)
+    # Scatter plots
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+
+    # First plot with the change ratio
+    ax1.scatter(x1, y)
+    ax1.set_xlabel("Change Ratio")
+    ax1.set_ylabel("% Change in input")
+
+    # Second plot with the % delta in hashes
+    ax2.scatter(x2, y)
+    ax2.set_xlabel("% Change in Hash")
+    ax2.set_ylabel("% Change in input")
+
+    # Making the plot square
     # lims = [
     #   np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
     #   np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
@@ -183,14 +150,103 @@ def main():
     # ax.set_xlim(lims)
     # ax.set_ylim(lims)
 
-    plt.axhline(y=10, color='r', linestyle='dashed')
-    plt.axhline(y=0, color='r', linestyle='dashed')
-    plt.axvline(x=20, color='r', linestyle='dashed')
-    plt.axvline(x=50, color='r', linestyle='dashed')
-    plt.xlabel("% Change in hash value")
-    plt.ylabel("% Change in input signature")
+    # Marking ranges of interest and drawing an x=y line for scale
+    for ax in (ax1, ax2):
+        ax.axline((0, 0), slope=1)
+        ax.axhline(y=10, color='r', linestyle='dashed')
+        ax.axhline(y=0, color='r', linestyle='dashed')
+        ax.axvline(x=20, color='r', linestyle='dashed')
+        ax.axvline(x=50, color='r', linestyle='dashed')
+
+    fig.savefig("scatter_tlsh_{}.png".format(fname), dpi=600)
     plt.show()
-    # fig.savefig('scatter_tlsh_p0f1.png', dpi=300)
+
+    return
+
+
+# Function to create an output log for the selected hashing scheme
+def process_file(input_file, ch=1):
+    input_file_path = ".\p0f signatures\{}".format(input_file)
+    # Read signatures from the input file
+    f = open(input_file_path, '+r')
+    lines = f.readlines()
+    f.close()
+
+    # Getting the right output file
+    if ch == 1:
+        output_file = open("tlsh_log.txt", 'w+')
+    elif ch == 2:
+        output_file = open("ssdeep_log.txt", 'w+')
+    elif ch == 3:
+        output_file = open("sdhash_log.txt", 'w+')
+    else:
+        print("Invalid input. Exiting.\n")
+        sys.exit(0)
+
+    # number of modified signatures on which gap stats have been calculated
+    num = 0
+
+    # initialize gap stats accumulators
+    mean_gap = 0
+    var_gap = 0
+
+    # initialize a list of locality data per signature
+    crlist = []
+
+    # lists for plotting
+    delta_input_list = []
+    delta_hash_list = []
+
+    print("Please wait for the computation to be over...")
+
+    for line in lines:
+        sign = line.strip()
+        output_file.write("Current sign: " + sign + '\n')
+        sign_hash = compute_hash(sign, ch)
+        for i in range(1, 20):
+            mod_sign = rand_change(sign)
+
+            # Calculating modified hash
+            mod_sign_hash = compute_hash(mod_sign, ch)
+
+            # Calculating locality, gap data
+            change_ratio = change_per_pos(
+                sign, mod_sign, sign_hash, mod_sign_hash, delta_input_list, delta_hash_list)
+            (mean_gap, var_gap) = calculate_gap_stats(
+                mean_gap, var_gap, num, sign_hash, mod_sign_hash)
+
+            # Writing computed stats to file
+            output_file.write(
+                f"Modified sign no.{i}: \n Change ratio: {str(change_ratio)}, Gap stats mean: {mean_gap}, variance: {var_gap}\n")
+            crlist.append(change_ratio)
+            num += 1
+
+    # Write aggregate stats in the output log
+    output_file.write("Mean change ratio:" +
+                      str(np.mean(crlist)) + "\n")
+    output_file.write("Input distribution mean: " + str(np.mean(delta_input_list)
+                                                        ) + " variance: " + str(np.var(delta_input_list)) + "\n")
+    output_file.write("Output distribution mean: " + str(np.mean(delta_hash_list)
+                                                         ) + " variance: " + str(np.var(delta_hash_list)) + "\n")
+
+    output_file.close()
+
+    # Plot the data
+    get_plots(input_file, crlist, delta_input_list, delta_hash_list)
+    return
+
+
+# main function
+def main():
+    # Get the name of the p0f signature file from command line
+    filename = sys.argv[1]
+
+    # print("Choose the hashing algorithm to run:\n ")
+    # print(" 1. TLSH \n 2. ssdeep \n 3. sdhash\n")
+    # ch = int(input("Enter choice:"))
+
+    process_file(filename, 1)
+    return
 
 
 if __name__ == "__main__":
